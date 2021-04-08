1b489e88f077e06df6dcd26ead62af976fa68d22
Maarten86M <maartenmoelands@gmail.com>
2021-04-08 16:55:41 +0200

    opdrachten afgerond zonder fouten

--- math-methods.js
+++ math-methods.js
@@ -9,18 +9,21 @@ console.log(Math.PI);
 // Tip: de formule is omtrek = diameter * pi
 // ---- Verwachte uitkomst: 12.566370614359172
 
+const cirkel = 4 * Math.PI;
+console.log(cirkel);
 
 /* Opdracht 2: */
 // Bereken de omtrek van een cirkel met een diameter van 12 en rond dit af naar een heel getal. Log dit in de terminal.
 // ---- Verwachte uitkomst: 38
-
-
+const cirkel2 = Math.round (12 * Math.PI);
+console.log(cirkel2);
 
 /* Opdracht 3: */
 // Bereken de wortel van 9 en log dit in de terminal
 // ---- Verwachte uitkomst: 3
 
-
+const wortel = Math.sqrt(9);
+console.log(wortel);
 
 /* Opdracht 4: */
 // Genereer een willekeurig getal tussen de 0 en 1 en doe dan een check:
@@ -28,3 +31,12 @@ console.log(Math.PI);
 // * is het willekeurige getal kleiner dan 0.5 ? Log dan het getal met "Het glas is half leeg" in de terminal
 // Tip: iedere keer als je het script opnieuw draait (wel even een extra spatie toevoegen o.i.d.) is dit getal anders ;)
 // ---- Verwachte uitkomst: Bijbehorende zin afhankelijk van de uitkomst
+
+const randomNumer = Math.random();
+if (randomNumer > 0.5){
+    console.log("Het glas is halfvol");
+}
+if (randomNumer <0.5) {
+    console.log("Het glas is half leeg")
+}
+console.log(randomNumer);
